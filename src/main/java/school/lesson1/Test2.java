a8c2f083c828c019ead7d505622434b5c902b84e
Пользователь <mozhilka@mail.ru>
2021-07-28 16:12:13 +0300

    First Commit

--- src/main/java/school/lesson1/Test2.java
+++ src/main/java/school/lesson1/Test2.java
@@ -3,18 +3,42 @@ package school.lesson1;
 public class Test2 {
     public static void main(String[] args) {
         printColor();
+        checkSumSign();
+        compareNumbers();
     }
 
     public static void printColor() {
-        int value = -15;
-        if (value <= 0 || value == 0) {
+        int value = -1;
+        if (value <= 0) {
             System.out.println("Красный");
-        }
-        if (value > 0 || value <= 100) {
+        } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
-        }
-        if (!(value > 100 && value <100)) {
+        } else {
             System.out.println("Зеленый");
         }
     }
-}
+    public static void checkSumSign() {
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+
+        if (a - b < 0) {
+            System.out.println("- Сумма отрицательная");
+        }
+    }
+
+    public static void compareNumbers() {
+            int a = 2;
+            int b = 1;
+
+            if (a >= b) {
+                System.out.println("a >= b");
+            } else {
+                System.out.println("a<b");
+            }
+        }
+    }
