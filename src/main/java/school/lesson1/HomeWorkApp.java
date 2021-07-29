9bce5f3812e26651e79c9fa1b0727f079e128aa3
Пользователь <mozhilka@mail.ru>
2021-07-29 11:06:01 +0300

    First Commit

--- src/main/java/school/lesson1/HomeWorkApp.java
+++ src/main/java/school/lesson1/HomeWorkApp.java
@@ -39,8 +39,8 @@ public class HomeWorkApp {
     }
 
     public static void compareNumbers() {
-            int a = 2;
-            int b = 1;
+            int a = 1;
+            int b = 2;
 
             if (a >= b) {
                 System.out.println("a >= b");
---
+++ src/main/java/school/lesson2/Test2.java
@@ -0,0 +1,14 @@
+package school.lesson2;
+
+public class Test2 {
+    public static void main(String[] args) {
+         int a = 5;
+         int b = 10;
+            boolean summe = true;
+            if ( a + b > 10 && a+b <= 20)
+                System.out.println ("true");
+            else {
+                summe = false;
+            }
+    }
+}
